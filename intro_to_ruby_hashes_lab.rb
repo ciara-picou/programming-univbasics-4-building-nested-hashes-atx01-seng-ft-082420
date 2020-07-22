45ea2cf8baa2a2062b58585ad928f6cd098a72c0
Ciara Picou <ciarapicou@att.net>
2020-07-22 17:37:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,8 @@ def monopoly_with_third_tier
    base_hash = {:railroads => {
    :pieces => 4 ,
    :rent_in_dollars => {
-     :one_piece_owned
+     :one_piece_owned => 25 , 
+     
    } ,
    :names => {}
    }
